f3feb98ef07fb8411b9a855542f3f6d35e4162dd
creatorofrealitiess <bitesofroro@gmail.com>
2026-03-29 12:09:59 -0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@
 // BOOKTOK STUDIO — SERVICE WORKER
 // Bump the version number to force cache refresh
 // =============================================
-const CACHE_NAME = 'booktok-v11';
+const CACHE_NAME = 'booktok-v12';
 const ASSETS = [
   './',
   './index.html',
